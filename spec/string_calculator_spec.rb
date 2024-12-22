9e1bb90d6b47dff4768fc9eb33417d437b6c5e24
shubham-yhakur-96 <55637919+shubham-yhakur-96@users.noreply.github.com>
2024-12-22 13:15:47 +0530

    Update string_calculator_spec.rb
    - Added `require_relative '../src/string_calculator'` to ensure correct loading of StringCalculator class
    - Ensured that the test can correctly access the StringCalculator implementation
    - Added RSpec test for handling a single number input in StringCalculator#add
    - The test will fail as the `add` method currently does not return the input number

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,10 +1,18 @@
 require 'rspec'
+require_relative '../src/string_calculator'
+
 RSpec.describe StringCalculator do
   describe '#add' do
     context 'when the string is empty' do
       it 'returns 0' do
         calculator = StringCalculator.new
         expect(calculator.add('')).to eq(0)
+      end
+    end
+    context 'when the string contains a single number' do
+      it 'returns the number itself' do
+        calculator = StringCalculator.new
+        expect(calculator.add('1')).to eq(1)
       end
     end
   end
